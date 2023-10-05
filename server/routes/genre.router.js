3899a6e36716d58f2bd0f3d735bff44f18b65489
Lucas Martin <driftpvp@gmail.com>
2023-10-05 17:00:03 -0500

    genre from db sql created, images manifest on details page. Work on details and genre manifestation.

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,17 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
+  const query = `SELECT * FROM "movies"
+  JOIN "movies_genres" ON "movies"."id"="movies_genres"."movie_id"
+  JOIN "genres" ON "genres"."id"="movies_genres"."genre_id";`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all genres', err);
   res.sendStatus(500)
+    })
 });
 
 module.exports = router;
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
-function Details () {
 
-    return(
-        <div>
+function Details() {
+
+    const dispatch = useDispatch();
+    const movies = useSelector(store => store.movies);
+    const history = useHistory();
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_GENRES' });
+    }, []);
+
+    return (
+        <main>
             <h1>Details Page</h1>
-        </div>
-    )
-};
+            <section className="genres">
+                {movies.map(movie => {
+                    return (
+                        <div key={movie.id} >
+                            <h3>{movie.title}</h3>
+                            <img src={movie.poster} alt={movie.title} onClick={()=> history.push("/")}/>
+                        </div>
+                    );
+                })}
+            </section>
+        </main>
+
+    );
+}
 
 export default Details;
